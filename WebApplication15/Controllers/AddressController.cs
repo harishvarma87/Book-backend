cbbf5b430288244a7280f5e5f46162bd9e230893
harishvarma87 <66619552+harishvarma87@users.noreply.github.com>
2022-09-29 14:01:37 +0530

    Add files via upload

---
+++ WebApplication15/Controllers/AddressController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication15.Models;
+
+namespace WebApplication15.Controllers
+{
+    public class AddressController : ApiController
+    {
+        private IAddressRepository repository;
+
+        public AddressController()
+        {
+            repository = new AddressSqlImp();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var data = repository.GetAllAddresses();
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetById(int id)
+        {
+            var data = repository.GetAddressByUser(id);
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(Address address)
+        {
+            repository.AddAddress(address);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Address address)
+        {
+            repository.UpdateAddress(address);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            repository.DeleteAddress(id);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication15/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication15.Models;
+
+namespace WebApplication15.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private IOrdersRepository repository;
+
+        public OrdersController()
+        {
+            repository = new OrdersSqlImp();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var data = repository.GetOrder();
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add(Orders order)
+        {
+            repository.AddOrder(order);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            repository.DeleteOrder(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Orders order)
+        {
+            repository.UpdateOrder(order);
+            return Ok();
+        }
+    }
+}
